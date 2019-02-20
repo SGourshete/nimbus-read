cd2b3a762aa57e83973d6fbc293b153de217a389
SGourshete <swapnilggourshete@gmail.com>
2019-02-20 13:21:35 +0530

    Added csv import method
    Added method to return number of records read from file

--- lib/nimbus_read.rb
+++ lib/nimbus_read.rb
@@ -1,5 +1,21 @@
+require 'csv'
+
 class NimbusRead
   def self.hi
     puts "Hi, this is Nimbus!"
+  end
+
+  attr_accessor :file
+
+  def initialize(path)
+    @file = CSV.read path
+  end
+
+  def read
+    file = CSV.read(path, headers: true)
+  end
+
+  def record_length
+    file.length
   end
 end
